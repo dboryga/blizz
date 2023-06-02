dbe348df64eb4537f1023c162adc9665d4b7871b
dboryga <dboryga@gmail.com>
2023-06-02 22:34:22 +0200

    flattenObject: fix separators mutation

--- libs/ui/src/lib/utils/flattenObject.ts
+++ libs/ui/src/lib/utils/flattenObject.ts
@@ -2,18 +2,18 @@ export const flattenObject = (
   obj: any,
   separators: string | string[] = '.',
   prefix = '',
-): { [k: string]: string } => {
-  console.log(obj);
+): Record<string, string> => {
   if (!obj) return {};
   let flatten = {};
-  const separator = Array.isArray(separators) ? separators.shift() : separators;
+  const _separators = [...separators];
+  const separator = Array.isArray(_separators) ? _separators.shift() : _separators;
 
   for (const key in obj) {
     const value = obj[key];
     const newKey = prefix ? `${prefix}${separator}${key}` : key;
 
     if (typeof value === 'object') {
-      Object.assign(flatten, flattenObject(value, separators, newKey));
+      Object.assign(flatten, flattenObject(value, _separators, newKey));
     } else {
       flatten = { ...flatten, [newKey]: value };
     }
